470e8bef5f18566738ad482dd8bad0b2e1a4a570
Winston Tokuhisa <tokuhisawinston@outlook.com>
2020-08-30 07:43:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,12 +17,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
-
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
-
   # return the projects hash
   projects
 end
