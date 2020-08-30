6d0e48ec036fd13ec54b494a94dff9819d73d654
Winston Tokuhisa <tokuhisawinston@outlook.com>
2020-08-30 07:32:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,4 @@
 # require libraries/modules here
-require "nokogiri"
-require "pry"
 
 def create_project_hash
   # This just opens a file and reads it into a variable
@@ -10,9 +8,8 @@ def create_project_hash
 
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
+
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
   binding.pry
 end
-
-create_project_hash
